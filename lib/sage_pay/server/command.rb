93840c8713946d4c2bf1a862ace41c1d712f29b1
Christian Batchelor <chris@batchelorassociates.co.uk>
2016-07-16 10:51:54 +0100

    Changed vps_protocol back to 2.23

--- lib/sage_pay/server/command.rb
+++ lib/sage_pay/server/command.rb
@@ -5,7 +5,7 @@ module SagePay
 
       class_attribute :tx_type, :vps_protocol
 
-      self.vps_protocol = "3.00" # was 2.23"
+      self.vps_protocol = "2.23" # was 2.23, tried 3.00
 
       attr_accessor :mode, :vendor, :vendor_tx_code
 
